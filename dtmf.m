94abc184235772c22b2083a3e11d3c2d8b2a40ba
Cuong Luong <ndrw_luong@yahoo.com>
2016-03-31 21:18:36 -0700

    Added Comments

--- dtmf.m
+++ dtmf.m
@@ -1,8 +1,11 @@
-function [] = dtmf(key)
-
+function [] = dtmf(key, duration)
+% Plays telephone dial tone for key number 0-9 for 'duration' seconds
+    
 %% Generate DTMF Tones
-Ts = 1/44100;
-t = 0:Ts:1;
+Ts = 1/44100; % Sampling period
+t = 0:Ts:duration;   
+
+% Generate table of dual dial tones for each key
 freq1 = zeros(4,3);
 freq2 = zeros(4,3);
 freq1(:,1) = 1209;
@@ -17,10 +20,17 @@ freq1 = freq1(:);
 freq2 = freq2';
 freq2 = freq2(:);
 
+% Key for zero assigned to 11 instead
+if (key == 0)
+    key = 11;
+end
+
+% Select appropriate combination of dial tones for specified key
 freq1 = freq1(key);
 freq2 = freq2(key);
-
 tone = cos(2*pi*freq1*t) + cos(2*pi*freq2*t);
+
+% Play dial tone for key
 soundsc(tone,44100);
 
 end
--- findnumber.m
+++ findnumber.m
@@ -1,25 +1,31 @@
+%% Outputs the two dominant frequencies per frame
 clc; clear; close all;
-%% Read in audio
-audio = audioread('dtmf.wav');
+
+% Read in audio
+[audio, Fs] = audioread('dtmf.wav');
+
+% Parameters
 framelen = 882;
 numwin = ceil(length(audio)/framelen);
-Fs = 44100;
 hamwin = hamming(framelen);
 freqmat = zeros(numwin,2);
 fftsize = 2048;
+
 % Zero pad
 audio = [audio; zeros(framelen*numwin-length(audio),1)];
+
+% Window by window processing
 for i = 1:numwin
     frame = audio((i-1)*framelen + 1:i*framelen).*hamwin;
     fftmag = abs(fft(frame, fftsize));
     fftmag = fftmag(1:fftsize/2);
+    % Find two largest peaks in FFT
     [maxima, loc] = findpeaks(fftmag);
     [maxima,ind] = sort(maxima,'descend');
     loc = loc(ind);
     if isempty(loc)
-        freqmat(i,:) = 0;
+        freqmat(i,:) = 0; % If current frame doesn't have peaks in FFT
     else
-        freqmat(i,:) = loc(1:2)*Fs/fftsize;
+        freqmat(i,:) = loc(1:2)*Fs/fftsize; % Store frequencies of 2 peaks per frame
     end
-    
 end
